5c22e46ccfbb8b57a9f9457d4fc3d42aee23d131
TKC <tarunkc22@gmail.com>
2025-01-27 00:11:27 -0500

    Added account section and heading

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -13,12 +13,26 @@ const StyledHeader = styled.header`
   justify-content: flex-end;
 `;
 
+const MainHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 function Header() {
   return (
-    <StyledHeader>
-      <UserAvatar />
-      <HeaderMenu />
-    </StyledHeader>
+    <MainHeader>
+      <h2>M O U N T &nbsp;&nbsp;&nbsp;V I E W</h2>
+      <StyledHeader>
+        <UserAvatar />
+        <HeaderMenu />
+      </StyledHeader>
+    </MainHeader>
   );
 }
 
